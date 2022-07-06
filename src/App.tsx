9ee578372341a7e1e0afa6670a11a315ad96f344
glebskr <glebskr@gmail.com>
2022-07-06 18:52:25 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/lib/table';
 import './App.css';
 
+const columns = [
+  {
+    title: 'Protocol',
+    dataIndex: 'name',
+    key: 'name',
+    render: (val: any, obj: any) => (
+      <a target={'_blank'} href={obj.url}>
+        {val}
+      </a>
+    ),
+  },
+  {
+    title: 'TVL',
+    dataIndex: 'tvl',
+    key: 'tvl',
+  },
+
+  {
+    title: 'Yields adapter',
+    dataIndex: 'yields',
+    key: 'yields',
+    render: (val: any, obj: any) => {
+      if (val) return <Tag color={'green'}>YES</Tag>;
+      return <Tag color={'red'}>NO</Tag>;
+    },
+  },
+];
+
 function App() {
+  const [adaptors, setAdaptors] = useState([]);
+  const [protocols, setProtocols] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://api.github.com/repos/DefiLlama/yield-server/git/trees/master?recursive=1'
+    )
+      .then((res) =>
+        res.json().then((res) =>
+          setAdaptors(
+            res.tree
+              .filter(
+                ({ path }: any) =>
+                  path.includes('adaptors') &&
+                  path.includes('index.js')
+              )
+              .map((adaptor: any) => ({
+                ...adaptor,
+                slug: adaptor.path.split('/')[2],
+              }))
+          )
+        )
+      )
+      .then(() => {
+        fetch('https://api.llama.fi/protocols').then((res) =>
+          res.json().then((protocolsRes) => {
+            const normalizedProtocols = protocolsRes
+              .sort((protocolA: any, protocolB: any) =>
+                protocolA.tvl > protocolB.tvl ? -1 : 1
+              )
+              .filter(
+                (protocol: any) =>
+                  !['Chain', 'Bridge'].includes(protocol.category)
+              )
+              .map((protocol: any) => ({
+                ...protocol,
+                yields: !!adaptors.find(
+                  (adaptor: any) => adaptor.slug === protocol.slug
+                ),
+              }));
+            setProtocols(normalizedProtocols);
+          })
+        );
+      });
+  }, [adaptors.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ marginBottom: 20 }}>
+        DefiLlama yields adapters coverage
+      </h1>
+      <Table
+        columns={columns}
+        dataSource={protocols}
+        rowClassName={(record: any, index) =>
+          record.yields ? '' : 'uncovered'
+        }
+        pagination={{ pageSize: 20 }}
+      />
     </div>
   );
 }
